d20de31799a3f61378cca09bf7aa6244e7f792c2
lisang <ranchangxin@gmail.com>
2023-11-15 20:12:04 +0800

    优化客户端的心跳检测和断开连接逻辑

--- go-task-server/internal/common/client_manager.go
+++ go-task-server/internal/common/client_manager.go
@@ -6,21 +6,31 @@ import (
 	"time"
 )
 
-type TaskClient struct {
-	Name      string  `json:"name"`      // 实例名称
-	Address   string  `json:"address"`   // 实例地址 ip:端口
-	Cpu       float64 `json:"cpu"`       // cpu使用率
-	Memory    float64 `json:"memory"`    // 内存使用率
-	Disk      float64 `json:"disk"`      // 硬盘使用率
-	RoundTime uint    `json:"roundTime"` // 上报时间间隔
+type ClientRegi struct {
+	Name      string `json:"name"`      // 任务节点名称
+	Port      uint   `json:"port"`      // 任务节点端口
+	Intervals uint   `json:"intervals"` // 任务节点心跳时间间隔
+}
+
+type TaskClientInfo struct {
+	UsedCpu     float64 `json:"UsedCpu"`     // CPU使用率
+	TotalMemory int64   `json:"TotalMemory"` // 总内存
+	UsedMemory  int64   `json:"usedMemory"`  // 使用内存
+	TotalDisk   int64   `json:"totalDisk"`   // 总硬盘空间
+	UsedDisk    int64   `json:"usedDisk"`    // 已使用内存空间
+	IpAddress   string  `json:"ipAddress"`   // 实例IP地址
 }
 
 type ClientItem struct {
-	ID         string     `json:"id"`
+	ID         string     `json:"id"`         // 实例Id
+	Name       string     `json:"name"`       // 实例名称
+	Address    string     `json:"address"`    // 实例地址
+	Intervals  uint       `json:"intervals"`  // 实例心跳间隔时间
 	OnlineTime *time.Time `json:"onlineTime"` // 实例的最后在线时间
 	Status     uint       `json:"status"`     // 0：正常 1：离线（超过心跳时间没有发送心跳请求
+	Channel    chan int   // 关闭连接的通道
 	roundFunc  func(string, uint)
-	*TaskClient
+	*TaskClientInfo
 }
 
 var (
@@ -39,17 +49,21 @@ func SaveClient(key string, client *ClientItem) {
 		retryCount := 0
 		for range ticker.C {
 			lastOnlineTime := getClientOnlineTime(key)
+			if lastOnlineTime == nil {
+				log.Error("客户端被删除，结束心跳检测")
+				break
+			}
 			if time.Now().Sub(*lastOnlineTime) > maxTimeout {
 				log.Warn("客户端心跳超时")
 				retryCount++
 				UpdateClientStatus(key, 1)
 				if retryCount >= 3 {
-					log.Errorf("客户端心跳超时3次，删除客户端，key:%v\n", key)
+					log.Warnf("客户端心跳超时3次，删除客户端，关闭连接，key:%v\n", key)
 					DeleteClient(key)
 					break
 				}
 			} else {
-				log.Infof("客户端心跳在线，key:%v, 最新时间：%v\n", key, lastOnlineTime)
+				log.Debugf("客户端心跳在线，key:%v, 最新时间：%v\n", key, lastOnlineTime)
 				if retryCount > 0 {
 					UpdateClientStatus(key, 0)
 					retryCount = 0
@@ -57,7 +71,7 @@ func SaveClient(key string, client *ClientItem) {
 			}
 		}
 	}
-	go client.roundFunc(key, client.RoundTime)
+	go client.roundFunc(key, client.Intervals)
 	mu.Lock()
 	defer mu.Unlock()
 	clientManager[key] = client
@@ -72,7 +86,7 @@ func getClientOnlineTime(key string) *time.Time {
 	return client.OnlineTime
 }
 
-func UpdateClientInfo(key string, info *TaskClient) {
+func UpdateClientInfo(key string, info *TaskClientInfo) {
 	mu.Lock()
 	defer mu.Unlock()
 	client, ok := clientManager[key]
@@ -81,7 +95,7 @@ func UpdateClientInfo(key string, info *TaskClient) {
 		return
 	}
 	now := time.Now()
-	client.TaskClient = info
+	client.TaskClientInfo = info
 	client.OnlineTime = &now
 }
 
@@ -96,7 +110,12 @@ func UpdateClientStatus(key string, status uint) {
 }
 
 func DeleteClient(key string) {
-	mu.Lock()
-	defer mu.Unlock()
-	delete(clientManager, key)
+	client, ok := clientManager[key]
+	if ok {
+		log.Infof("删除客户端，key：%v\n", key)
+		mu.Lock()
+		close(client.Channel)
+		delete(clientManager, key)
+		mu.Unlock()
+	}
 }
--- go-task-server/internal/handler/websocket_handler.go
+++ go-task-server/internal/handler/websocket_handler.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2/log"
 	"go-task-server/internal/common"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -16,32 +18,56 @@ func NewWebSocketHandler() *WebSocketHandler {
 
 func (w *WebSocketHandler) WebSocket(c *websocket.Conn) {
 	clientId := c.Params("id")
-	clientInfo := &common.TaskClient{}
-	for {
-		err := c.ReadJSON(clientInfo)
-		if err != nil {
-			log.Errorf("客户端连接失败或消息解析失败，删除客户端关闭连接，错误消息：%v\n", err)
-			common.DeleteClient(clientId)
-			_ = c.Close()
-			break
-		}
-		client := common.GetClient(clientId)
-		if client == nil {
-			now := time.Now()
-			client = &common.ClientItem{
-				ID:         clientId,
-				OnlineTime: &now,
-				Status:     0,
-				TaskClient: clientInfo,
-			}
-			log.Infof("第一次连接，添加客户端，clientId:%v\n", clientId)
-			common.SaveClient(clientId, client)
-		} else {
-			log.Infof("客户端上报信息续约，更新客户端信息，clientId:%v\n", clientId)
-			common.UpdateClientInfo(clientId, clientInfo)
-		}
-		if err = c.WriteMessage(1, []byte("ok")); err != nil {
-			log.Errorf("服务端返回消息失败，错误信息：%v\n", err)
-		}
+	address := c.RemoteAddr().String()
+	clientName := c.Query("name")
+	intervalsStr := c.Query("intervals", "5")
+	port := c.Query("port")
+	intervals, err := strconv.ParseUint(intervalsStr, 10, 32)
+	if len(clientName) <= 0 || err != nil {
+		log.Errorf("客户端连接参数错误，关闭连接,clientName：%v，err：%v\n", clientName, err)
+		_ = c.Close()
+		return
 	}
+	index := strings.LastIndex(address, ":")
+	now := time.Now()
+	address = address[:index] + ":" + port
+	channel := make(chan int)
+	client := &common.ClientItem{
+		ID:         clientId,
+		Name:       clientName,
+		Intervals:  uint(intervals),
+		Address:    address,
+		OnlineTime: &now,
+		Status:     0,
+		Channel:    channel,
+	}
+	log.Infof("%v\n", client)
+	log.Infof("客户端连接成功，保存客户端，clientId:%v\n", clientId)
+	common.SaveClient(clientId, client)
+	clientInfo := &common.TaskClientInfo{}
+
+	go func(clientId string) {
+		for {
+			err = c.ReadJSON(clientInfo)
+			if err != nil {
+				log.Errorf("客户端连接失败或消息解析失败，删除客户端关闭连接，错误消息：%v\n", err)
+				break
+			}
+			client = common.GetClient(clientId)
+			if client == nil {
+				log.Error("客户端被删除，上报信息无效，关闭连接")
+				break
+			}
+			log.Debugf("客户端上报信息续约，更新客户端信息，clientId:%v\n", clientId)
+			common.UpdateClientInfo(clientId, clientInfo)
+			if err = c.WriteMessage(1, []byte("ok")); err != nil {
+				log.Errorf("服务端返回消息失败，错误信息：%v\n", err)
+			}
+		}
+		common.DeleteClient(clientId)
+	}(clientId)
+
+	_ = <-channel
+	log.Info("监听到关闭通道信息，关闭连接")
+	_ = c.Close()
 }
